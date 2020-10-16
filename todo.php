d8f921afde4df5e5fa24506ad29938b613e78468
hiroshikado <okonomiyakunarahiroimono@yahoo.co.jp>
2020-10-16 19:53:17 +0900

    php-code

--- todo.php
+++ todo.php
@@ -18,4 +18,27 @@ if(file_exists($FILE)) {
   $BOARD = json_decode(file_get_contents($FILE));
 }
 
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+  if(!empty($_POST['txt'])){
+      $text = $_POST['txt'];
+      $DATA = [$id, $date, $text];
+      $BOARD[] = $DATA;
+
+      file_put_contents($FILE, json_encode($BOARD));
+
+  }else if(isset($_POST['del'])){
+      $NEWBOARD = [];
+
+      foreach($BOARD as $DATA){
+          if($DATA[0] !== $_POST['del']){
+              $NEWBOARD[] = $DATA;
+          }
+      }
+      file_put_contents($FILE, json_encode($NEWBOARD));
+  }
+  
+  header('Location: '.$_SERVER['SCRIPT_NAME']);
+  exit;
+}
+
 ?>
