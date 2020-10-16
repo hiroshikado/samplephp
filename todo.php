1ac952441b79519655a50bc26dc59ae5f472448c
hiroshikado <okonomiyakunarahiroimono@yahoo.co.jp>
2020-10-16 10:28:58 +0900

    value-def

--- todo.php
+++ todo.php
@@ -1,3 +1,21 @@
 <?php
 
+function h($v){
+  return htmlspecialchars($v, ENT_QUOTES, 'UTF-8');
+}
+
+$FILE = 'todo.txt';
+$id = uniqid();
+
+date_default_timezone_set('Japan');
+$date = date('Y年m月d日H時i分');
+
+$text = '';
+$DATA = [];
+$BOARD = [];
+
+if(file_exists($FILE)) {
+  $BOARD = json_decode(file_get_contents($FILE));
+}
+
 ?>
