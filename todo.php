3ffb5a5168c8ae257db4d31a7b75f821d2f56046
hiroshikado <okonomiyakunarahiroimono@yahoo.co.jp>
2020-10-21 23:29:50 +0900

    class-trying

--- todo.php
+++ todo.php
@@ -57,7 +57,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
   <section class= "main">
       <h2>ToDoAppに投稿する</h2>
 
-      <form method= "post">
+      <form method= "post", class = 't'>
           <input type= "text" name= "txt">
           <input type= "submit" value= "投稿">
       </form>    
