490aea8125ef55f162370e8c53abbd2269dbecd1
hiroshikado <okonomiyakunarahiroimono@yahoo.co.jp>
2020-11-12 19:44:56 +0900

    comp

--- index.php
+++ index.php
@@ -10,20 +10,15 @@
     <?php
       $url = "https://www.sejuku.net/blog/";
       
-      //cURLセッションを初期化する
       $ch = curl_init();
         
-      //URLとオプションを指定する
       curl_setopt($ch, CURLOPT_URL, $url);
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         
-      //URLの情報を取得する
       $res =  curl_exec($ch);
         
-      //結果を表示する
       var_dump($res);
         
-      //セッションを終了する
       curl_close($conn);
      ?>
   </section>
