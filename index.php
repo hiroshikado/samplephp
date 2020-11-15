11614419f4a7a805cd50ad1a584b652fe0bf2911
hiroshikado <okonomiyakunarahiroimono@yahoo.co.jp>
2020-11-15 20:13:13 +0900

    cookie-trying

--- index.php
+++ index.php
@@ -8,13 +8,8 @@
   <h1>PHP7技術者認定試験(初級)用</h1>
   <section class= "main">
     <?php
-      <form action="#" method="post">
-      <input type="text" name="mail" />
-      <input type="submit" name="" value="送信">
-      </form>
-
-      $mail = filter_input(INPUT_POST, 'mail', FILTER_VALIDATE_EMAIL);
-      var_dump($mail);
-     ?>
+      setcookie(‘username’,’yamada’,time()+60*60*24*7);
+      echo $_COOKIE[‘username’];
+    ?>
   </section>
 </body>
