f944ef6023d5dbc63ed737573c3686b5b677de43
hiroshikado <okonomiyakunarahiroimono@yahoo.co.jp>
2020-11-17 16:36:12 +0900

    mb_str-trying

--- index.php
+++ index.php
@@ -8,19 +8,21 @@
   <h1>PHP7技術者認定試験(初級)用</h1>
   <section class= "main">
     <?php
-      // 連想配列($array)
-      $array = array(
-        "name" => "あらゆ" ,
-        "gender" => "男" ,
-        "blog" => array(
-          "name" => "SYNCER" ,
-          "published" => "2014-06-10" ,
-          "url" => "https://syncer.jp/" ,
-        ),
-      );
+      /* 文字列を定義 */
+      $str = "AbCdEfG";
+      $mb_str = "pHp 入門！";
+      
+      /* 大文字に変換 */
+      var_dump( strtoupper($str) );
+      echo "<br />";
+      var_dump( mb_strtoupper($mb_str) );
+      echo "<br />";
 
-      // 連想配列($array)をJSONに変換(エンコード)する
-      $json = json_encode( $array ) ;
+      /* 小文字に変換 */
+      var_dump( strtolower($str) );
+      echo "<br />";
+      var_dump( strtolower($mb_str) );
+      echo "<br />";
     ?>
   </section>
 </body>
