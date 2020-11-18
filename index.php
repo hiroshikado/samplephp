f72021457746cb26c54afa80044eeb394ae0960d
hiroshikado <okonomiyakunarahiroimono@yahoo.co.jp>
2020-11-18 16:17:41 +0900

    try-catch_trying

--- index.php
+++ index.php
@@ -8,21 +8,27 @@
   <h1>PHP7技術者認定試験(初級)用</h1>
   <section class= "main">
     <?php
-      /* 文字列を定義 */
-      $str = "AbCdEfG";
-      $mb_str = "pHp 入門！";
-      
-      /* 大文字に変換 */
-      var_dump( strtoupper($str) );
-      echo "<br />";
-      var_dump( mb_strtoupper($mb_str) );
+      // ２つの引数の合計値を返却する
+      function sum($num1, $num2) {
+        // 引数が数値かを判定する
+        if (is_numeric($num1) && is_numeric($num2)) {
+            return $num1 + $num2;
+        }
+
+        // いずれかの引数が数値以外の場合
+        throw new InvalidArgumentException('引数には数値以外を設定しないでください');
+      }
+
+      // 引数がどちらも正常なので 1+2 の結果である3が表示されます
+      echo sum(1, 2).PHP_EOL;
       echo "<br />";
 
-      /* 小文字に変換 */
-      var_dump( strtolower($str) );
-      echo "<br />";
-      var_dump( strtolower($mb_str) );
-      echo "<br />";
+      // 引数に数値以外を指定すると例外が発生するので、それをキャッチしてエラーメッセージを表示します
+      try {
+        sum('ABC', 2).PHP_EOL;
+      } catch (InvalidArgumentException $e) {
+        echo $e->getMessage().PHP_EOL;
+      }
     ?>
   </section>
 </body>
