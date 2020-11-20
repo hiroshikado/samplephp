3d48d7c1b5a22ecf80aeb09835ace780fdc611e6
hiroshikado <okonomiyakunarahiroimono@yahoo.co.jp>
2020-11-21 02:49:34 +0900

    curl-trying

--- index.php
+++ index.php
@@ -8,8 +8,23 @@
   <h1>PHP7技術者認定試験(初級)用</h1>
   <section class= "main">
     <?php
-      setcookie(‘username’,’山田’,time()+60*60*24*7);
-      echo $_COOKIE[‘username’];
+      $url = "https://www.sejuku.net/blog/";
+
+      //cURLセッションを初期化する
+      $ch = curl_init();
+
+      //URLとオプションを指定する
+      curl_setopt($ch, CURLOPT_URL, $url);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+      //URLの情報を取得する
+      $res =  curl_exec($ch);
+
+      //結果を表示する
+      var_dump($res);
+
+      //セッションを終了する
+      curl_close($conn);
     ?>
   </section>
 </body>
