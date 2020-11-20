60ff52a775195af1dd224165adfbbe1484df7795
hiroshikado <okonomiyakunarahiroimono@yahoo.co.jp>
2020-11-20 10:21:57 +0900

    to-japanese

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <h1>PHP7技術者認定試験(初級)用</h1>
   <section class= "main">
     <?php
-      setcookie(‘username’,’yamada’,time()+60*60*24*7);
+      setcookie(‘username’,’山田’,time()+60*60*24*7);
       echo $_COOKIE[‘username’];
     ?>
   </section>
