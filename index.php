c7b603b0bfa4aa8bd53184af36212acc2249fca0
hiroshikado <okonomiyakunarahiroimono@yahoo.co.jp>
2020-11-22 23:08:18 +0900

    datetime-tryung

--- index.php
+++ index.php
@@ -8,23 +8,9 @@
   <h1>PHP7技術者認定試験(初級)用</h1>
   <section class= "main">
     <?php
-      $url = "https://www.sejuku.net/blog/";
+      $today = date("Y-m-d H:i:s");
 
-      //cURLセッションを初期化する
-      $ch = curl_init();
-
-      //URLとオプションを指定する
-      curl_setopt($ch, CURLOPT_URL, $url);
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-      //URLの情報を取得する
-      $res =  curl_exec($ch);
-
-      //結果を表示する
-      var_dump($res);
-
-      //セッションを終了する
-      curl_close($conn);
+      print_r($today);
     ?>
   </section>
 </body>
